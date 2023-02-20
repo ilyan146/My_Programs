da0cbd48cf5b59363418424d392a3321bd916094
ilyan146 <ilyan146@gmail.com>
2023-02-21 00:18:04 +0400

    Day 22: Add app descriptions, images, and links.

--- Programs/app2-personal_portfolio/main.py
+++ Programs/app2-personal_portfolio/main.py
@@ -21,12 +21,18 @@ st.write("Below you find some of the apps I have built in Python, Feel free to c
 
 df = pd.read_csv("006 data.csv")
 
-col3, col4 = st.columns(2)
+col3, empty_column, col4 = st.columns([1.5, 0.5, 1.5])
 
 with col3:
     for index, row in df[0:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("006 images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("006 images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
