4eba1942bef2520fb06ac27c9ba13f0a692372d9
ilyan146 <ilyan146@gmail.com>
2023-02-20 21:07:59 +0400

    Create columns, images, and description

---
+++ Programs/app2-personal_portfolio/main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("006 images\photo.png")
+
+with col2:
+    st.title("Mohamed Ilyan")
+    content = """
+    This is a trial content description and we will be making a nice summary of my experience and skillset here!!
+    Also try to see how the content of this is displayed in the browser!!
+    """
+    st.info(content)
