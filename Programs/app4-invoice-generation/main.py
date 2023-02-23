787adc647ee2c64c2a4e5fb9c44cf4f602a8ccdc
ilyan146 <ilyan146@gmail.com>
2023-02-23 21:10:54 +0400

    Day 26: Add invoice date.

--- Programs/app4-invoice-generation/main.py
+++ Programs/app4-invoice-generation/main.py
@@ -11,9 +11,15 @@ for i in filepaths:
     df = pd.read_excel(i, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(i).stem
     invoice_nr = filename.split("-")[0][4:]
+    inv_date = filename.split("-")[1]
+
     pdf.set_font(family="Times", style="B", size=16)
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
+
+    pdf.set_font(family="Times", style="B", size=16)
+    pdf.cell(w=50, h=8, txt=f"Date: {inv_date}", ln=2)
 
     output = pdf.output(f"PDFs/{filename[4:]}.pdf")
