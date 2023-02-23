1ccb4d4ab0d9fe388b59eb030eba6221d0d92de4
ilyan146 <ilyan146@gmail.com>
2023-02-23 22:26:15 +0400

    Day 26: Add a table in invoice.

--- Programs/app4-invoice-generation/main.py
+++ Programs/app4-invoice-generation/main.py
@@ -8,7 +8,7 @@ filepaths = glob.glob("invoices/*.xlsx")
 print(filepaths)
 
 for i in filepaths:
-    df = pd.read_excel(i, sheet_name="Sheet 1")
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
 
@@ -20,6 +20,32 @@ for i in filepaths:
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
 
     pdf.set_font(family="Times", style="B", size=16)
-    pdf.cell(w=50, h=8, txt=f"Date: {inv_date}", ln=2)
+    pdf.cell(w=50, h=8, txt=f"Date: {inv_date}", ln=1)
+
+    pdf.ln(20)
+
+    df = pd.read_excel(i, sheet_name="Sheet 1")
+
+    column = df.columns
+    headers = [i.replace("_"," ").title() for i in column]
+
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=headers[0],border=1)
+    pdf.cell(w=70, h=8, txt=headers[1],border=1)
+    pdf.cell(w=35, h=8, txt=headers[2],border=1)
+    pdf.cell(w=30, h=8, txt=headers[3],border=1)
+    pdf.cell(w=30, h=8, txt=headers[4],border=1, ln=1)
+
+
+    for index,row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80,80,80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=35, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+
 
     output = pdf.output(f"PDFs/{filename[4:]}.pdf")
