f99a0884c81afe47f9e91633f6083f0445e3b36a
ilyan146 <ilyan146@gmail.com>
2023-02-25 19:38:57 +0400

    Day 28: Add links, specify language, limit news, add mail subject, control topic.

--- Programs/app5-new-api-email/main.py
+++ Programs/app5-new-api-email/main.py
@@ -1,39 +1,30 @@
 import requests
 from send_email import send_email
+from datetime import date
+today = date.today()
 
-
+topic = "engineering"
 
 key = "715f2e1394254e7baeba8eb87fddab91"
-url = "https://newsapi.org/v2/everything?q=tesla&from=" \
-      "2023-01-25&sortBy=publishedAt&apiKey=715f2e1394254e7baeba8eb87fddab91"
+url = f"https://newsapi.org/v2/everything?q={topic}&from={today}&" \
+      "sortBy=publishedAt&apiKey=715f2e1394254e7baeba8eb87fddab91&language=en"
+
 
 request = requests.get(url)
 content = request.json()
 
-#message = """\
-#Subject: News headers and descriptions
-
-#"""
-"""message = []
-for index,i in enumerate(content["articles"]):
-    text = f"{index+1} {i['title']} \n {i['description']}"
-    new_text = text.split("\n")
-    message.append(new_text)
-print(message)"""
-
 message = ""
-for index,i in enumerate(content["articles"]):
+for index,i in enumerate(content["articles"][:25]):
     #Below is for case when the tile of article is kept as None, which will cause error.
     if i["title"] is not None:
-        message += f"{index+1} {i['title']} \n{i['description']} \n\n"
+        message += f"{index+1}--{i['title']}\n{i['description']}\n{i['url']}\n\n"
 
 
-final_message = """\
-Subject: Email from API, topics and description
+final_message = f"""\
+Subject: Email from API Topic: {topic.capitalize()}
 
 """ + message
 
-print(final_message)
 
 # Need to encode due to code error on str format
 send_email(final_message.encode("utf-8"))
