36ddd828b0ab87e5c93c801572b563e5db811c6e
ilyan146 <ilyan146@gmail.com>
2023-02-25 19:16:26 +0400

    Day 27: Send an email with news titles and descriptions.

--- Programs/app5-new-api-email/main.py
+++ Programs/app5-new-api-email/main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 
 
@@ -9,6 +10,33 @@ url = "https://newsapi.org/v2/everything?q=tesla&from=" \
 request = requests.get(url)
 content = request.json()
 
+#message = """\
+#Subject: News headers and descriptions
+
+#"""
+"""message = []
 for index,i in enumerate(content["articles"]):
-    print(index+1, i["title"])
-    print(i["description"])
+    text = f"{index+1} {i['title']} \n {i['description']}"
+    new_text = text.split("\n")
+    message.append(new_text)
+print(message)"""
+
+message = ""
+for index,i in enumerate(content["articles"]):
+    #Below is for case when the tile of article is kept as None, which will cause error.
+    if i["title"] is not None:
+        message += f"{index+1} {i['title']} \n{i['description']} \n\n"
+
+
+final_message = """\
+Subject: Email from API, topics and description
+
+""" + message
+
+print(final_message)
+
+# Need to encode due to code error on str format
+send_email(final_message.encode("utf-8"))
+
+
+
